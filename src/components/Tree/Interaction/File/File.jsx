4abf9218919c861b386c622567167121408afeee
Vladislav Zakharov <vlad@Vladislavs-MacBook-Pro-2.local>
2022-08-31 16:35:25 -0700

    relead file state from online

--- src/components/Tree/Interaction/File/File.jsx
+++ src/components/Tree/Interaction/File/File.jsx
@@ -1,12 +1,15 @@
 import './File.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FileManager } from '../../../../data/FileManager'
 
 const File = () => {
   const file = useSelector(state => state.treeReducer.file)
   const [currentFile, setCurrentFile] = useState(file)
-
+  useEffect(() => {
+    setCurrentFile(file)
+  })
+  
   return (
     <div className='file'>
       <textarea type="text"
